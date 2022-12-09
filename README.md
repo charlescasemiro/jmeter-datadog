96a85e3208c18ed94dd2f0e8a564331dcd83123b
Charles Casemiro <charlescasemiro@Charless-MacBook-Pro.local>
2022-12-09 03:39:50 -0300

    upload files

---
+++ README.md
@@ -0,0 +1,13 @@
+## CONTAINER PARA TESTE COM JMETER E DATADOG AGENT ##
+
+Para build da imagem executar o script abaixo:
+
+./build.sh
+
+Para executar o teste com JMETER:
+
+./test.sh
+
+Caso queira executar em background rodar:
+
+nohup ./test.sh &
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Run JMeter Docker image with options
+
+NAME="jmeter$(echo $RANDOM $RANDOM $RANDOM $RANDOM $RANDOM | md5 | cut -c -8)"
+JMETER_VERSION=${JMETER_VERSION:-"latest"}
+IMAGE="justb4/jmeter:5.5"
+
+# Finally run
+docker run --rm --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
